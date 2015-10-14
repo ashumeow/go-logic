2b968b392198f4a565b27d1c4efb09a25a93b09c
ashumeow <ashumeow@live.com>
2015-10-15 01:47:39 +0530

    add shell sort

--- algorithms/sorting/shell.go
+++ algorithms/sorting/shell.go
@@ -8,6 +8,15 @@ import (
 )
 
 func main {
-
-	// still more to add...
+	meow := meow_sort.RandArray(10)
+	fmt.Println("Given array is: ", meow)
+	fmt.Println("")
+	for ig_ig := int(len(arr)/2); ig_ig > 0; ig_ig /= 2 {
+		for x := ig_ig; x < len(meow); x++ {
+			for xx := x; xx >= ig_ig && meow[xx - ig_ig] > meow[xx]; xx -= ig_ig {
+				meow[xx], meow[meow - ig_ig] = meow[xx - ig_ig], meow[xx]
+			}
+		}
+	}
+	fmt.Println("Sorted array is: ", meow)
 }
