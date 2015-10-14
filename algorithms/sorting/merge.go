d28c127293a74535e6ed93eea80fd5e8d0b916e9
ashumeow <ashumeow@live.com>
2015-10-15 01:31:36 +0530

    add merge sort

--- algorithms/sorting/merge.go
+++ algorithms/sorting/merge.go
@@ -7,4 +7,40 @@ import (
 	"ashumeow/meow_sort"
 )
 
-// still more to add
+func merge(left, right []int) []int {
+	out := make([]int,0,len(left)+len(right))
+
+	for len(left) > 0 || len(right) > 0 {
+		if len(left) == 0 {
+			return append(out, right...)
+		}
+		if len(right) == 0 {
+			return append(out, left...)
+		}
+		if left[0] <= right[0] {
+			out = append(out, left[0])
+			left = left[1:]
+		} else {
+			out = append(out, right[0])
+			right = right[1:]
+		}
+	}
+	return out
+}
+
+func merge_ig(meow []int) []int {
+	if len(meow) <= 1 {
+		return meow
+	}
+	middle := len(meow) / 2
+	left := merge_ig(meow[:middle])
+	right := merge_ig(meow[:middle])
+	return merge(left, right)
+}
+
+func main() {
+	meow := meow_sort.RandArray(10)
+	fmt.Println("Given array is: ",meow)
+	fmt.Println("")
+	fmt.Println("Sorted array is: ",merge_ig(meow))
+}
