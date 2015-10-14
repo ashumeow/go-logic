265dce1515a10ffb2407f3e361b8849961324b3d
ashumeow <ashumeow@live.com>
2015-10-15 02:20:57 +0530

    add selection sort

--- algorithms/sorting/selection.go
+++ algorithms/sorting/selection.go
@@ -8,5 +8,23 @@ import (
 )
 
 func main() {
-	// still more to code...
+	meow := meow_sort.RandArray(10)
+	fmt.Println("Given array is: ", meow)
+	fmt.Println("")
+
+	var minimum int = 0
+	var temp int = 0
+
+	for x := 0; x < len(meow_sort); x++ {
+		minimum = x
+		for xx := x + 1; xx < len(meow); xx++ {
+			if meow[xx] < meow[minimum] {
+				minimum = xx
+			}
+		}
+		temp = meow[x]
+		meow[x] = meow[minimum]
+		meow[minimum] = temp
+	}
+	fmt.Println("Sorted array is: ", meow)
 }
