18298c3dda3dd16d184f544f426ec320a9470cde
ashumeow <ashumeow@live.com>
2015-10-15 04:06:40 +0530

    update

--- data-structures/trees/binary-search/binary_search.go
+++ data-structures/trees/binary-search/binary_search.go
@@ -21,3 +21,28 @@ func new_binary_search_tree(value interface{}, comp CompareFunc) *binary_search_
 }
 
 // comparing 2 interfaces
+type CompareFunc func(x1, x5 interface{}) int
+
+// searching
+func(ig *binary_search_tree) meowSearch(x interface{}) interface{} {
+	meowDir := ig.compare(ig.meowNode, x)
+
+	switch meowDir {
+	case 0:
+		return ig.meowNode
+	case -1:
+		if ig.right==nil {
+			return nil
+		} else
+		return ig.right.meowSearch(x)
+	case 1:
+		if ig.left==nil {
+			return nil
+		} else 
+		return ig.left.meowSearch(x)
+	default:
+		panic("invalid compare function")
+	}
+}
+
+// checks for key existence
